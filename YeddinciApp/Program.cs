f344d50757d768b2622bc25a50e33939acad24ec
QemerS <tu201806169@code.edu.az>
2021-10-26 04:32:09 +0400

    Add project files.

---
+++ YeddinciApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace YeddinciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Write a C# program that takes two numbers as input
+            and perform an operatinon (+ - * /) on them
+            and displays the result of that operation.
+             */
+
+            Console.WriteLine("Input first number: "); int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input operationr: "); string c = Console.ReadLine();
+            Console.WriteLine("Input second number: "); int b = Convert.ToInt32(Console.ReadLine());
+            switch (c)
+            {
+                case "+":
+                    Console.WriteLine(Convert.ToString(a) + Convert.ToString(c) + Convert.ToString(b) + "=" + (a + b));
+                    break;
+                case "-":
+                    Console.WriteLine(Convert.ToString(a) + Convert.ToString(c) + Convert.ToString(b) + "=" + (a - b));
+                    break;
+                case "*" :
+                    Console.WriteLine(Convert.ToString(a) + Convert.ToString(c) + Convert.ToString(b) + "=" + (a * b));
+                    break;
+                case "/" :
+                    Console.WriteLine(Convert.ToString(a) + Convert.ToString(c) + Convert.ToString(b) + "=" + (a / b));
+                    break;
+            } Console.WriteLine("You entered something wrong");
+        }
+    }
+}
